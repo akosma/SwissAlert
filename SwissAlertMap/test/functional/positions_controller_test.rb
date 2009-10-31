8da40fa50e318c94186b5c8c2eaf3291e8bfcfe3
Adrian Kosmaczewski <akosma@gmail.com>
2009-10-31 11:37:27 +0100

    Added new tests to the suite

--- SwissAlertMap/test/functional/positions_controller_test.rb
+++ SwissAlertMap/test/functional/positions_controller_test.rb
@@ -7,17 +7,118 @@ class PositionsControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:positions)
   end
+  
+  test "if index controller has a code parameter, it redirects to the show action" do
+    get :index, :code => "ABC"
+    assert_redirected_to position_url("ABC")
+  end
+  
+  test "when requesting on a normal browser, a map is drawn" do
+    get :index
+    assert_not_nil assigns(:map)
+    assert_not_nil assigns(:positions)
+    
+    get :show, :code => "ABC"
+    assert_not_nil assigns(:map)
+    assert_not_nil assigns(:positions)
+  end
+  
+  test "when requesting json, only the list is generated" do
+    @request.env["HTTP_ACCEPT"] = "application/javascript"
+    get :index
+    assert_nil assigns(:map)
+    assert_not_nil assigns(:positions)    
 
-  test "should create position" do
+    @request.env["HTTP_ACCEPT"] = "application/javascript"
+    get :show, :code => "ABC"
+    assert_nil assigns(:map)
+    assert_not_nil assigns(:positions)    
+  end
+
+  test "when requesting xml, only the list is generated" do
+    @request.env["HTTP_ACCEPT"] = "text/xml"
+    get :index
+    assert_nil assigns(:map)
+    assert_not_nil assigns(:positions)    
+
+    @request.env["HTTP_ACCEPT"] = "text/xml"
+    get :show, :code => "ABC"
+    assert_nil assigns(:map)
+    assert_not_nil assigns(:positions)    
+  end
+
+  test "should create position with a correct POST request" do
     assert_difference('Position.count') do
       post :create, :latitude => 10.0, :longitude => 10.0
     end
+    assert_response :redirect
+    assert_redirected_to position_url(assigns(:position).code)
   end
 
-  test "should show position" do
-    get :show, :code => "ehe4rf"
+  test "should redirect to the index action for invalid POST creation requests" do
+    post :create, :latitude => nil, :longitude => "abs"
+    assert_response :redirect
+    assert_redirected_to root_url
+  end
+  
+  test "should allow to create items with XML requests" do
+    @request.env["HTTP_ACCEPT"] = "text/xml"
+    assert_difference('Position.count') do
+      post :create, :latitude => 10.0, :longitude => 10.0
+    end
+    assert_equal 0, assigns(:position).errors.count
+    assert_not_equal 0, @response.body.length
+    assert_response :created
+  end
+
+  test "should output errors in invalid XML creation requests" do
+    @request.env["HTTP_ACCEPT"] = "text/xml"
+    post :create, :latitude => "blah", :longitude => nil
+    assert_not_equal 0, assigns(:position).errors.count
+    assert_not_equal 0, @response.body.length
+    assert_response :unprocessable_entity
+  end
+
+  test "should allow to create items with JSON requests" do
+    @request.env["HTTP_ACCEPT"] = "application/javascript"
+    assert_difference('Position.count') do
+      post :create, :latitude => 10.0, :longitude => 10.0
+    end
+    assert_equal 0, assigns(:position).errors.count
+    assert_response :created
+    assert_not_equal 0, @response.body.length
+  end
+
+  test "should output errors in invalid JSON creation requests" do
+    @request.env["HTTP_ACCEPT"] = "application/javascript"
+    post :create, :latitude => "blah", :longitude => nil
+    assert_not_equal 0, assigns(:position).errors.count
+    assert_not_equal 0, @response.body.length
+    assert_response :unprocessable_entity
+  end
+
+  test "should show position for existing items" do
+    get :show, :code => "EHERF"
     assert_response :success
     assert_not_nil assigns(:position)
+    assert_equal 1, assigns(:positions).count
+  end
+  
+  test "should display all items when requesting for a nonexisting item" do
+    get :show, :code => "whatever_non_existent"
+    assert_response :success
+    assert assigns(:positions).count > 1
   end
 
+  test "should expose a 404 code for nonexistent items when doing XML requests" do
+    @request.env["HTTP_ACCEPT"] = "text/xml"
+    get :show, :code => "whatever_non_existent"
+    assert_response :not_found
+  end
+
+  test "should expose a 404 code for nonexistent items when doing JSON requests" do
+    @request.env["HTTP_ACCEPT"] = "application/javascript"
+    get :show, :code => "whatever_non_existent"
+    assert_response :not_found
+  end
 end
--- SwissAlertMap/test/unit/position_test.rb
+++ SwissAlertMap/test/unit/position_test.rb
@@ -7,5 +7,55 @@ class PositionTest < ActiveSupport::TestCase
     position.save
     assert_not_nil position.code
   end
+  
+  test "generates a spell phrase for instances" do
+    position = Position.new
+    position.save
+    assert_not_nil position.spell
+  end
+  
+  test "the latitude and the longitude are required" do
+    position = Position.new
+    position.save
+    assert_not_equal 0, position.errors.count
+
+    position = Position.new
+    position.latitude = "2.34"
+    position.save
+    assert_not_equal 0, position.errors.count
+
+    position = Position.new
+    position.longitude = "2.34"
+    position.save
+    assert_not_equal 0, position.errors.count
+
+    position = Position.new
+    position.latitude = "2.34"
+    position.longitude = "2.34"
+    position.save
+    assert_equal 0, position.errors.count
+  end
+  
+  test "the latitude and the longitude must be numbers" do
+    position = Position.new
+    position.save
+    assert_not_equal 0, position.errors.count
+
+    position = Position.new
+    position.latitude = "aaaaaa"
+    position.save
+    assert_not_equal 0, position.errors.count
+
+    position = Position.new
+    position.longitude = "bbbbbbbb"
+    position.save
+    assert_not_equal 0, position.errors.count
+
+    position = Position.new
+    position.latitude = "2.34"
+    position.longitude = "2.34"
+    position.save
+    assert_equal 0, position.errors.count
+  end
 
 end
